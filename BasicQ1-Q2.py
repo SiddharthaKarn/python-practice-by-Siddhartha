032733faacb0d6a3169b5e980f10fec1e654e3aa
SiddharthaKarn <Siddhartha248720@gmail.com>
2026-05-07 10:47:51 +0530

    Add basic questions for variable and arithmetic operations

---
+++ BasicQ1-Q2.py
@@ -0,0 +1,16 @@
+# ========== BASIC QUESTIONS ==========
+
+# Question 1:
+# Create a variable named 'name' and store your name in it. Then print it.
+name = "Siddhartha"
+print("Name:",name)
+
+# Question 2:
+# Create two integer variables 'x = 15' and 'y = 4'. Print their sum, difference, product, and quotient.
+x = 15
+y = 4
+sum = x + y
+difference = x - y 
+product = x * y
+quocient = x / y
+print("Sum:",sum,"\ndifference:",difference,"\nproduct:",product,"\nquocient:",quocient)    
