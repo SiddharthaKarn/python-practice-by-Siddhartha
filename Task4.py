94edbcd6677162d7d391cf7ff0e972f6b4173553
SiddharthaKarn <Siddhartha248720@gmail.com>
2026-05-07 10:15:15 +0530

    Add user input for name and age with welcome message

---
+++ Task4.py
@@ -0,0 +1,4 @@
+# Task 4=Take a name and age as input from the user and prin a welcome message 
+name=input("Enter Your name:--")
+age=input("Enter Your age:--")
+print("Welcome",name,"You are",age,"Years old")
