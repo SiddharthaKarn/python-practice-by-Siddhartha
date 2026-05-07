6273a50d0722fbf403867bd04717623a41bbacf4
SiddharthaKarn <Siddhartha248720@gmail.com>
2026-05-07 10:15:40 +0530

    Create Task5.py

---
+++ Task5.py
@@ -0,0 +1,6 @@
+#Task5:You bought 3 items --Rs.50.5,Rs25 and Rs30,calculate the total cost of allthe items 
+item_1=50.5
+item_2=25
+item_3=30
+total=item_1+item_2+item_3
+print("The total of all item is:--",total)
