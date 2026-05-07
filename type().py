b91a5423e5ab2f571ae0d2ac2083ad301b366728
SiddharthaKarn <Siddhartha248720@gmail.com>
2026-05-07 11:21:37 +0530

    Create type().py

---
+++ type().py
@@ -0,0 +1,20 @@
+# Question :
+# Use the type() function to check and print the data type of: 25, 3.14, 3+4j, "Python", list, tuple, Map, True and set 
+x = 25
+print("X:",x,"\nDatatype:",type(x))
+y = 3.14
+print("Y:",y,"\nDatatype:",type(y))
+z = 3+4j
+print("Z:",z,"\nDatatype:",type(z))
+s = "Python"
+print("S:",s,"\nDatatype:",type(s))
+l = [10,20,30,10,70]
+print("L:",l,"\nDatatype:",type(l))
+t = (1,2,3)
+print("T",t,"\nDatatype:",type(t))
+myDist = {"name" :"Siddhartha","age": 25}
+print("dictionary:",myDist,"\nDatatype",type(myDist))
+a = True
+print("A:",a,"\nDatatype:",type(a))
+st = {19,28,18,48,10,8,10}
+print("SET:",st,"\nDatatype",type(st))
