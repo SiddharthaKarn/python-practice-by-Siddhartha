1ac27d16aaa30becdbeb58315b3f8ffe6636014f
SiddharthaKarn <Siddhartha248720@gmail.com>
2026-05-07 10:10:20 +0530

    Add Task1.py to print personal information

---
+++ Task1.py
@@ -0,0 +1,7 @@
+# Task 1= print your name,age and favorite language using variables
+name = "Siddhartha"
+age = 17
+language = "Python"
+print("My name is",name)
+print("My age is",age)
+print("My favorite language is",language)
